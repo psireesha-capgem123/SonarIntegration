9fd65b521c03629a59605837ea39a680851225c5
psireesha-capgeminidotcom <sireesha.pappala@capgemini.com>
2020-05-21 17:40:02 +0530

    Sonar commit

--- src/main/java/com/example/demo/SonarCloudJenkinsApplication.java
+++ src/main/java/com/example/demo/SonarCloudJenkinsApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SonarCloudJenkinsApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SonarCloudJenkinsApplication.class, args);
+		
 	}
 
 }
